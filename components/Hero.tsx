729842c3ab4ad0f7cf3b6b46ebe6dbd55b8831f7
Aesan De Silva <aesanclyde.desilva@gmail.com>
2025-03-14 17:26:44 +0800

    revert(hero): restore previous hero section layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,44 +6,44 @@ import { FaLocationArrow } from 'react-icons/fa6'
 
 const Hero = () => {
   return (
-    <div className="relative min-h-screen flex flex-col justify-center items-center">
-      {/* Spotlights */}
-      <div>
-        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
-      </div>
+    <div className='pb-20 pt-36'>
+        <div>
+            <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+            <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
+            <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+        </div>
 
-      {/* Background Overlay */}
-      <div className="absolute inset-0 w-full h-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.02] bg-grid-black/[0.2] flex items-center justify-center">
-        <div className="absolute inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-      </div>
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.02] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        </div>
 
-      {/* Content */}
-      <div className="relative z-10 max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center text-center">
-        <h2 className="uppercase tracking-widest text-xs text-blue-100 max-w-80">
-          Your vision, My Code
-        </h2>
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+              Your vision, My Code
+            </h2>
 
-        <TextGenerateEffect 
-          className="text-[40px] md:text-5xl lg:text-6xl"
-          words="Creating clean, user-friendly sites that drive results"
-        />
-        
-        <p className="md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-          Hi, I&apos;m Aesan, a Web Developer
-        </p>
+            <TextGenerateEffect 
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              words="Creating clean, user-friendly sites that drive results"
+            />
+            
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+              Hi, I&apos;m Aesan, a Web Developer
+            </p>
 
-        <a href="#about">
-          <MagicButton 
-            title="Show my work"
-            icon={<FaLocationArrow />}
-            position="right"
-          />
-        </a>
-      </div>
+            <a href="#about">
+              <MagicButton 
+                title = "Show my work"
+                icon = {<FaLocationArrow />}
+                position='right'
+               />
+            </a>
+          </div>
+        </div>
     </div>
   )
 }
 
 export default Hero
+
