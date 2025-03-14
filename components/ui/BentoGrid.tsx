ec1fee187c7c28405df002eef910ce1505912f04
Aesan De Silva <aesanclyde.desilva@gmail.com>
2025-03-14 21:28:26 +0800

    cleanup(grid): remove unused div from BackgroundGradientAnimation

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -102,7 +102,6 @@ export const BentoGridItem = ({
         </div>
         {id === 6 &&(
             <BackgroundGradientAnimation>
-                <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
             </BackgroundGradientAnimation>
         )}
 
